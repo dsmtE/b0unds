bfb9d72ecd8ab56afbdbcc06f2001819936f99a7
dsmtE <desmet.enguerrand@gmail.com>
2022-08-14 13:36:18 +0200

    🩹 fix modulo isssue

--- shaders/rayMarchUtils.glsl
+++ shaders/rayMarchUtils.glsl
@@ -1,5 +1,15 @@
+float modulo(float x, float y) { return x - y * floor(x/y); }
+vec2  modulo(vec2  x, vec2  y) { return x - y * floor(x/y); }
+vec3  modulo(vec3  x, vec3  y) { return x - y * floor(x/y); }
+
 float sphereSDF(vec3 p, float radius) { return length(p) - radius; }
 
+// vec3 arrayOp(vec3 p, vec3 offset) {
+//     const vec3 halfOffset = 0.5 * offset;
+//     return mod(p + halfOffset, offset) - halfOffset;
+// }
 vec3 arrayOp(vec3 p, vec3 offset) {
-    return mod(p + 0.5 * offset, offset) - 0.5 * offset;
+    const vec3 halfOffset = 0.5 * offset;
+    return modulo(p + halfOffset, offset) - halfOffset;
 }
+
